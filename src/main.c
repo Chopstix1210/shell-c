c5bb45443bc3cac0ad964d2475a92e7e4e670513
kevinsegovia <kjsegovi@gmail.com>
2025-11-02 11:08:38 -0700

    set exit condition

--- src/main.c
+++ src/main.c
@@ -10,10 +10,21 @@ int main(int argc, char *argv[]) {
   while (true) {
     printf("$ ");
     char command[1024];
+    const char* delimiter = " ";
+
     fgets(command, sizeof(command), stdin);
+    const char *token = strtok(command, delimiter);
+
+    int exit_cmp = strcmp(token, "exit");
+    if (exit_cmp == 0) {
+        int status = atoi(strtok(NULL, delimiter));
+        exit(status);
+    }
+
 
     command[strcspn(command, "\n")] = '\0';
     printf("%s: command not found\n", command);
+
   }
     return 0;
 }
