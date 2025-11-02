2fefcafca530b0a31db7492231318585531d0390
kevinsegovia <kjsegovi@gmail.com>
2025-11-02 10:55:17 -0700

    setting while loop

--- src/main.c
+++ src/main.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  printf("$ ");
-  char command[1024];
-  fgets(command, sizeof(command), stdin);
+  while (true) {
+    printf("$ ");
+    char command[1024];
+    fgets(command, sizeof(command), stdin);
 
-  command[strcspn(command, "\n")] = '\0';
-  printf("%s: command not found\n", command);
-  return 0;
+    command[strcspn(command, "\n")] = '\0';
+    printf("%s: command not found\n", command);
+  }
+    return 0;
 }
