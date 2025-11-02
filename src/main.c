20a4cbf822fa035e7d938da700378bdf3a646018
kevinsegovia <kjsegovi@gmail.com>
2025-11-02 11:16:29 -0700

    new echo command added

--- src/main.c
+++ src/main.c
@@ -15,15 +15,21 @@ int main(int argc, char *argv[]) {
     fgets(command, sizeof(command), stdin);
     const char *token = strtok(command, delimiter);
 
+    // cmp known commands
     int exit_cmp = strcmp(token, "exit");
+    int echo_cmp = strcmp(token, "echo");
+
+
     if (exit_cmp == 0) {
         int status = atoi(strtok(NULL, delimiter));
         exit(status);
+    } else if (echo_cmp == 0) {
+        char *token = strtok(NULL, "\n");
+        printf("%s\n", token);
+    } else {
+        command[strcspn(command, "\n")] = '\0';
+        printf("%s: command not found\n", command);
     }
-
-
-    command[strcspn(command, "\n")] = '\0';
-    printf("%s: command not found\n", command);
 
   }
     return 0;
