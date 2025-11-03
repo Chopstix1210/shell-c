bdaee8e875164990472125af3b9b020acd088cc3
Kevin Segovia <kjsegovi@gmail.com>
2025-11-03 16:01:41 -0700

    added type handling

--- src/main.c
+++ src/main.c
@@ -3,34 +3,57 @@
 #include <string.h>
 #include <stdbool.h>
 
-int main(int argc, char *argv[]) {
-  // Flush after every printf
-  setbuf(stdout, NULL);
+void handle_echo(char *args) {
+    if (args)
+        printf("%s\n", args);
+}
 
-  while (true) {
-    printf("$ ");
-    char command[1024];
-    const char* delimiter = " ";
+void handle_exit(char *args) {
+    int code = args ? atoi(args) : 0;
+    exit(code);
+}
 
-    fgets(command, sizeof(command), stdin);
-    const char *token = strtok(command, delimiter);
-
-    // cmp known commands
-    int exit_cmp = strcmp(token, "exit");
-    int echo_cmp = strcmp(token, "echo");
-
-
-    if (exit_cmp == 0) {
-        int status = atoi(strtok(NULL, delimiter));
-        exit(status);
-    } else if (echo_cmp == 0) {
-        char *token = strtok(NULL, "\n");
-        printf("%s\n", token);
-    } else {
-        command[strcspn(command, "\n")] = '\0';
-        printf("%s: command not found\n", command);
+void handle_type(char *command, char *builtin_list[], int length) {
+    int found = 0;
+    for (int i=0; i < length; i++) {
+        if (strcmp(command, builtin_list[i]) == 0) {
+            found = 1;
+            break;
+        }
     }
 
-  }
+    if (found == 1) {
+        printf("%s is a shell builtin\n", command);
+    } else {
+        printf("%s: not found\n", command);
+    }
+}
+
+int main(void) {
+    setbuf(stdout, NULL);
+    char *builtin_commands[] = {"echo", "exit", "type"};
+
+    while (true) {
+        printf("$ ");
+        char command[1024];
+        fgets(command, sizeof(command), stdin);
+        command[strcspn(command, "\n")] = '\0'; // remove newline
+
+        char *token = strtok(command, " ");
+        if (!token) continue;
+
+        char *args = strtok(NULL, ""); // everything after first token
+
+        if (strcmp(token, "exit") == 0) {
+            handle_exit(args);
+        } else if (strcmp(token, "echo") == 0) {
+            handle_echo(args);
+        } else if (strcmp(token, "type") == 0) {
+            int length = sizeof(builtin_commands) / sizeof(builtin_commands[0]);
+            handle_type(args, builtin_commands, length);
+        }else {
+            printf("%s: command not found\n", token);
+        }
+    }
     return 0;
 }
