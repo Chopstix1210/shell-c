6c2c93f3bc67926916e90f04db54035a9312373b
Kevin Segovia <kjsegovi@gmail.com>
2025-11-10 16:17:51 -0700

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -3,8 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 
 char *concat(const char *s1, const char *s2) {
   char *concat_string = malloc(strlen(s1) + strlen(s2) + 2);
@@ -36,7 +37,7 @@ char *handle_path_search(char *command) {
     token = strtok(NULL, delimiter);
   }
   dirs[num_dirs] = "\0";
-  char *found = "\0";
+  char *found = NULL;
 
   for (int i = 0; dirs[i] != NULL; i++) {
     DIR *dp;
@@ -84,12 +85,50 @@ void handle_type(char *command, char *builtin_list[], int length) {
     printf("%s is a shell builtin\n", command);
   } else {
     char *found_path = handle_path_search(command);
-    if (strcmp(found_path, "\0") != 0) {
+    if (found_path != NULL) {
       printf("%s is %s\n", command, found_path);
     } else {
       printf("%s: not found\n", command);
     }
   }
+}
+
+
+int handle_execs(char* exe, char *args) {
+  char* exec_args[1024];
+
+  char* program_name = strrchr(exe, '/');
+  program_name = program_name ? program_name + 1 : exe;
+
+  exec_args[0] = malloc(strlen(program_name) + 1);
+  strcpy(exec_args[0], program_name);
+  int num_args = 1;
+
+  char* token = strtok(args, " ");
+  while (token != NULL) {
+    exec_args[num_args] = malloc(strlen(token) + 1);
+    strcpy(exec_args[num_args], token);
+    num_args++;
+    token = strtok(NULL, " ");
+  }
+  exec_args[num_args] = NULL;
+
+
+  pid_t child = fork();
+  // printf("args %s\n", args);
+  if (child == 0) {
+    execvp(exe, exec_args);
+    perror("execvp error");
+    exit(EXIT_FAILURE);
+  }
+  if (child > 0) {
+    wait(NULL);
+    //printf("back to parent");
+  } else {
+    perror("fork error");
+    exit(EXIT_FAILURE);
+  }
+  return 0;
 }
 
 int main(void) {
@@ -116,7 +155,12 @@ int main(void) {
       int length = sizeof(builtin_commands) / sizeof(builtin_commands[0]);
       handle_type(args, builtin_commands, length);
     } else {
-      printf("%s: command not found\n", token);
+      char* found = handle_path_search(command);
+      if (found != NULL) {
+        handle_execs(found, args);
+      } else {
+        printf("%s: command not found\n", token);
+      }
     }
   }
   return 0;
